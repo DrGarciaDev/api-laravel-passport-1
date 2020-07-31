0c8776034ffeae56c6f9ada30361b1102a2b9977
Luis <luis_grdz@hotmail.com>
2020-07-31 14:55:58 -0500

    Scalafolding for autencticate with laravel/ui vue

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Route::get('/client', function(){
     return view('client');
 })->middleware('auth');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
